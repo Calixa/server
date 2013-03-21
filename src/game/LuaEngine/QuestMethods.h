983ba5ff653380310491a940d49bb4ae61b527b6
Daniel S. Reichenbach <daniel@kogitoapp.com>
2013-03-21 13:08:47 +0100

    [z2309] Clean up CMake files, and add placeholders for the Lua engine.
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- src/game/LuaEngine/QuestMethods.h
+++ src/game/LuaEngine/QuestMethods.h
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright (C) 2005-2013 MaNGOS <http://getmangos.com/>
  * Copyright (C) 2009-2013 MaNGOSZero <https://github.com/mangoszero>
  *
@@ -16,5 +16,3 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
- 
- #include <lua.hpp>
